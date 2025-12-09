7206e430f36a0ad5353c429162d6c6b0f8c56a7b
saminstein <saminstein@yahoo.com>
2025-12-09 18:24:49 +0100

    Make 'share-option' hidden in normal state so that the footer section will be well aligned

--- app.js
+++ app.js
@@ -2,25 +2,6 @@ const social = document.querySelector('.card-author');
 
 const icon = document.querySelector('.icon');
 
-const share = document.querySelector('.share-option');
-
-
-
-
-
-
-
-
-// function toggleActive(e) {
-//   if (!e.target.className) {
-//     e.target.className = "active";
-//   } else {
-//     e.target.className = "";
-//   }
-// }
-
-
-// icon.addEventListener("click", toggleActive)
 
 icon.addEventListener('click', () => {
   icon.classList.toggle('active');
